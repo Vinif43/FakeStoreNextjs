48deab56b6aae6085a3b5d0fb37d494d04748856
Vinicius Ferreira Marinho <41167263+Vinif43@users.noreply.github.com>
2025-04-05 14:12:00 -0300

    fix: Corrige mensagem de erro no hook useCart

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -115,7 +115,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
 export const useCart = () => {
   const context = useContext(CartContext)
   if (!context) {
-    throw new Error('useCart deve ser usado dentro de um CartProvider')
+    throw new Error('useCart deve ser usado dentro de um CartProvider ')
   }
   return context
 }
