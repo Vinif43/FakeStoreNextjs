eecefe7ba5d600c67635928a9cae2350d89402eb
Vinicius Ferreira Marinho <41167263+Vinif43@users.noreply.github.com>
2025-04-05 14:56:35 -0300

    :lipstick: fix: Ajusta altura do loader para 80vh

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Loader() {
   return (
     <div>
-      <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex flex-col justify-center items-center h-[80vh]">
         <img src="/img/logo.png" alt="logo" className="w-56 animate-pulse" />
         {/* <div className="flex">
           <h1 className="text-3xl font-bold">Carregando</h1>
